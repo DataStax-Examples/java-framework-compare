2d28ba05cabcae5c4a70c46afb007af2f7151e83
tomekl007 <tomasz.lelek@datastax.com>
2020-08-19 11:59:26 +0200

    remove type-hint

--- micronaut-jdbc/src/main/java/lizzy/medium/compare/micronaut/Application.java
+++ micronaut-jdbc/src/main/java/lizzy/medium/compare/micronaut/Application.java
@@ -1,11 +1,7 @@
 package lizzy.medium.compare.micronaut;
 
-import io.micronaut.core.annotation.TypeHint;
 import io.micronaut.runtime.Micronaut;
 
-@TypeHint(typeNames = {
-        "org.postgresql.Driver",
-})
 public class Application {
 
     public static void main(String[] args) {
