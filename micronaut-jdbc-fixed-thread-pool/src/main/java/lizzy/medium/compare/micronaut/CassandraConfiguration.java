2975704297cc9515be619186f0dc83c70a788ca2
tomekl007 <tomasz.lelek@datastax.com>
2020-08-19 12:03:04 +0200

    adapt micronaut-jdbc-fixed-thread-pool

---
+++ micronaut-jdbc-fixed-thread-pool/src/main/java/lizzy/medium/compare/micronaut/CassandraConfiguration.java
@@ -0,0 +1,43 @@
+package lizzy.medium.compare.micronaut;
+
+import io.micronaut.context.annotation.ConfigurationProperties;
+
+@ConfigurationProperties("cassandra")
+public class CassandraConfiguration {
+	private String keyspace;
+	private String secureConnectBundle;
+	private String username;
+	private String password;
+
+	public String getKeyspace() {
+		return keyspace;
+	}
+
+	public void setKeyspace(String keyspace) {
+		this.keyspace = keyspace;
+	}
+
+	public String getSecureConnectBundle() {
+		return secureConnectBundle;
+	}
+
+	public void setSecureConnectBundle(String secureConnectBundle) {
+		this.secureConnectBundle = secureConnectBundle;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
--- micronaut-jdbc-fixed-thread-pool/src/main/java/lizzy/medium/compare/micronaut/Repository.java
+++ micronaut-jdbc-fixed-thread-pool/src/main/java/lizzy/medium/compare/micronaut/Repository.java
@@ -1,11 +1,16 @@
 package lizzy.medium.compare.micronaut;
 
-import io.micronaut.data.jdbc.annotation.JdbcRepository;
-import io.micronaut.data.model.query.builder.sql.Dialect;
-import io.micronaut.data.repository.CrudRepository;
-
+import java.util.Optional;
 import java.util.UUID;
 
-@JdbcRepository(dialect = Dialect.POSTGRES)
-interface Repository extends CrudRepository<Issue, UUID> {
+interface Repository {
+    Optional<Issue> findById(UUID id);
+
+    Issue insert(Issue body);
+
+    Issue update(Issue body);
+
+    void deleteById(UUID id);
+
+    Iterable<Issue> findAll();
 }
