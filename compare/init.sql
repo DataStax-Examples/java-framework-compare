d5f8da16083dc73173a4875f5d371767fab0cfb4
tomekl007 <tomasz.lelek@datastax.com>
2020-08-20 12:16:49 +0200

    remove init.sql

--- compare/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE issue (
-    id UUID PRIMARY KEY,
-    name VARCHAR(255),
-    description TEXT
-);
-
-INSERT INTO issue (id, name, description) VALUES('550e8400-e29b-11d4-a716-446655440000', 'Test', 'This is a test');
