627744ead1afd97cac553ca4a398e47dd9242555
tomekl007 <tomasz.lelek@datastax.com>
2020-08-20 11:07:18 +0200

    adapt spring-jdbc to java driver raw queries

--- spring-jdbc/src/main/java/lizzy/medium/compare/spring/RepositoryImpl.java
+++ spring-jdbc/src/main/java/lizzy/medium/compare/spring/RepositoryImpl.java
@@ -1,62 +1,60 @@
 package lizzy.medium.compare.spring;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Controller;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
-@Controller
-@RequiredArgsConstructor
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.cql.ResultSet;
+import com.datastax.oss.driver.api.core.cql.Row;
+import com.datastax.oss.driver.api.core.cql.SimpleStatement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class RepositoryImpl implements Repository {
-    private final JdbcTemplate jdbcTemplate;
+
+    private final CqlSession cqlSession;
+
+    @Autowired
+    public RepositoryImpl(CqlSession cqlSession) {
+        this.cqlSession = cqlSession;
+    }
 
     @Override
     public Optional<Issue> findById(UUID id) {
-        return Optional.ofNullable(jdbcTemplate.query("SELECT id, name, description FROM Issue WHERE id = ?",
-                new Object[]{id}, rs -> { rs.next(); return convert(rs);}));
+        return Optional.ofNullable(
+                cqlSession.execute(SimpleStatement.newInstance("SELECT * from issue where id = ?", id)).one()
+        ).map(this::mapToIssue);
     }
 
     @Override
     public Issue insert(Issue issue) {
-        jdbcTemplate.update("INSERT INTO Issue(id, name, description) VALUES (?,?,?)",
-                issue.getId(),
-                issue.getName(),
-                issue.getDescription());
-        return findById(issue.getId()).orElseThrow(RuntimeException::new);
+        cqlSession.execute(SimpleStatement.newInstance("INSERT INTO issue(id, name, description) VALUES (?,?,?)", issue.getId(), issue.getName(), issue.getDescription()));
+        return issue;
     }
 
     @Override
     public void deleteById(UUID id) {
-        jdbcTemplate.update("DELETE FROM Issue WHERE id = ?", id);
+        cqlSession.execute(SimpleStatement.newInstance("DELETE from issue where id = ?", id));
     }
 
     @Override
     public List<Issue> findAll() {
-        return jdbcTemplate.query("SELECT id, name, description FROM Issue", (rs, row) -> convert(rs));
+        ResultSet result = cqlSession.execute(SimpleStatement.newInstance("SELECT * FROM issue"));
+        return result.all().stream().map(this::mapToIssue).collect(Collectors.toList());
     }
 
     @Override
     public Issue update(Issue issue) {
-        jdbcTemplate.update("UPDATE Issue g SET name = ?, description = ? where id = ?",
-                issue.getName(),
-                issue.getDescription(),
-                issue.getId());
+        cqlSession.execute(SimpleStatement.newInstance("UPDATE issue SET name = ?, description = ? WHERE ID = ?",
+                issue.getName(), issue.getDescription(), issue.getId()));
         return findById(issue.getId()).orElseThrow(RuntimeException::new);
     }
 
-    private Issue convert(ResultSet rs) {
-        try {
-            UUID id = (UUID) rs.getObject("id");
-            String name = rs.getString("name");
-            String description = rs.getString("description");
-            return new Issue(id, name, description);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+
+    private Issue mapToIssue(Row row) {
+        return new Issue(row.getUuid("id"), row.getString("name"), row.getString("description"));
     }
 }
